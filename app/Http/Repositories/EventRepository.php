b4f064e8ab39231e77405d15036281ffd80465dc
BrunoGurris <brunoagpires@gmail.com>
2022-10-29 00:08:39 -0300

    feat(events): create

--- app/Http/Repositories/EventRepository.php
+++ app/Http/Repositories/EventRepository.php
@@ -7,19 +7,21 @@ use Exception;
 
 class EventRepository
 {
-    public function create($request, $user)
+    public function create($request, $user, $upload)
     {
         try {
             $event = new Event();
             $event->user_id = $user->id;
             $event->name = $request->name;
             $event->description = $request->description;
+            $event->date = date('Y-m-d', strtotime($request->date)).' '.date('H:i:s', strtotime($request->date));
             $event->zip_code = $request->zip_code;
             $event->street = $request->street;
             $event->number = $request->number;
             $event->neighborhood = $request->neighborhood;
             $event->city = $request->city;
             $event->state = $request->state;
+            $event->image = $upload;
             $event->save();
 
             return response()->json($event, 200);
